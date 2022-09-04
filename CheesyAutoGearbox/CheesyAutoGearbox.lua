c2b955ee49a5352656c0b5bae138caa79ed34222
CheesyManiac <cheesymaniac44@gmail.com>
2022-09-04 21:42:12 +0100

    Added a bunch of functions to the status app

--- CheesyAutoGearbox/CheesyAutoGearbox.lua
+++ CheesyAutoGearbox/CheesyAutoGearbox.lua
@@ -2,8 +2,6 @@ local changeUp, changeDown = 0, 0
 local car0 = ac.getCar(0)
 local carCar = ac.INIConfig.carData(0,'car.ini')
 local carEngine = ac.INIConfig.carData(0,'engine.ini')
---extconfig = ac.INIConfig.load('extension/config/cars/kunos/ks_audi_a1s1.ini'):get('LIGHT_HEADLIGHTS','BOUND_EXP',0)
-extconfig2 = ac.INIConfig.load('extension/config/cars/kunos/ks_audi_a1s1.ini'):setAndSave('LIGHT_HEADLIGHTS','BOUND_EXP',0)
 
 local shiftUpLUT = {['rpm']={-1.0,-0.8,-0.0,0.0,0.0,0.0,0.0,0.0,0.8,0.9,1.0}, ['throttle']={0.0,1.0,0.8,0.6,0.4,0.0,0.0,-0.4,-0.4,-0.3,-0.2}}
 
@@ -23,27 +21,31 @@ local function changeGear()
     end
 end
     local changeGearUp = 0
+    local susFront = 0.1
+    local susRear = 0.1
+    local forceUp = 0
 
 
+    local carPos = ac.getCar(0).position
 function script.windowMain(dt)
-    --ac.debug('test',extconfig)
+
     local gasUse = math.round((car0.gas*10)+1,0)
     local rpmFrac = math.round(((car0.rpm / car0.rpmLimiter)*10)+1,0)
 
-    ui.pushDWriteFont('Speed Racing:fonts/SpeedRacing.ttf')
-    ui.dwriteText('MINIMUM:  '..carEngine:get('ENGINE_DATA','MINIMUM')[1],24)
-    ui.dwriteText('LIMITER:  '..carEngine:get('ENGINE_DATA','LIMITER')[1])
-    ui.dwriteText('gearCount:  '..car0.gearCount)
-    ui.popDWriteFont()
+
+    ui.text('MINIMUM:  '..carEngine:get('ENGINE_DATA','MINIMUM')[1],24)
+    ui.text('LIMITER:  '..carEngine:get('ENGINE_DATA','LIMITER')[1])
+    ui.text('gearCount:  '..car0.gearCount)
+
 
     ui.separator()
     changeGear()
     ui.separator()
 
-    ui.dwriteText('rpm:   '..car0.rpm)
-    ui.dwriteText('rpm %:   '..rpmFrac)
-    ui.dwriteText('gas:   '..gasUse)
-    ui.dwriteText('gear:  '..car0.gear)
+    ui.text('rpm:   '..car0.rpm)
+    ui.text('rpm %:   '..rpmFrac)
+    ui.text('gas:   '..gasUse)
+    ui.text('gear:  '..car0.gear)
 
     ui.text(shiftUpLUT['rpm'][rpmFrac])
     ui.text(shiftUpLUT['throttle'][gasUse])
@@ -62,5 +64,4 @@ function script.windowMain(dt)
     else
         ac.store("changeDown", 0)
     end
-
 end
--- CheesyCarStatus/CheesyCarStatus.lua
+++ CheesyCarStatus/CheesyCarStatus.lua
@@ -1,13 +1,36 @@
-local wiperModes = ac.getCar().wiperModes
-local WiperSpeed, ABS, Gear = 0, 0, 0
+local car = ac.getCar(0)
 
---function ui.progressBar(fraction, size, overlay) end
+local carModes = {
+    ['Wipers'] = car.wiperModes,
+    ['ABS'] = car.absModes,
+    ['TC'] = car.tractionControlModes,
+    ['gearCount'] = car.gearCount
+}
+
+local Icons = {
+    ['EML'] = 'Icons/EML_ON.png',
+    ['ABS'] = 'Icons/ABS_ON.png',
+    ['ESC'] = 'Icons/ESC_ON.png',
+    ['T_COLD'] = 'Icons/TEMP_COLD.png',
+    ['T_HOT'] = 'Icons/TEMP_HOT.png',
+    ['INDICATORS'] = 'Icons/INDICATORS.png',
+    ['LIGHT_HIGHBEAM'] = 'Icons/LIGHT_HIGHBEAM.png',
+    ['LIGHT_PARKING'] = 'Icons/LIGHT_PARKINGpng.png'
+}
+
+local columnWidths = {
+    160,
+    150,
+    115,
+    160,
+    250
+}
 
 local function changeWiper()
-    ui.setColumnWidth(3, 70)
+    local WiperSpeed = car.wiperMode
     ui.text('   Wipers')
     if ui.arrowButton('increaseWiper', ui.Direction.Up, vec2(64,16)) then
-        if WiperSpeed < wiperModes then
+        if WiperSpeed < carModes.Wipers then
             WiperSpeed = WiperSpeed + 1
             ac.setWiperSpeed(0, WiperSpeed)
         end
@@ -25,102 +48,275 @@ local function changeWiper()
 end
 
 local function changeABS()
+    local absCurrent = car.absMode
+    ac.debug('absCurrent', absCurrent)
     ui.text('   ABS')
     if ui.arrowButton('increaseABS', ui.Direction.Up, vec2(64,16)) then
-        if ABS < 12 then
-            ABS = ABS + 1
-            ac.setABS(ABS)
+        if absCurrent < carModes.ABS then
+            absCurrent = absCurrent + 1
+            ac.setABS(absCurrent)
+        end
+    end
+    ui.button(absCurrent,vec2(64,24),ui.ButtonFlags.Disabled)
+    if ui.arrowButton('decreaseABS', ui.Direction.Down, vec2(64,16)) then
+        if absCurrent > 0 then
+            absCurrent = absCurrent - 1
+            ac.setABS(absCurrent)
         end
     end
 
-    ui.button(ABS,vec2(64,24),ui.ButtonFlags.Disabled)
+    ui.offsetCursor(vec2(75,-68))
 
-    if ui.arrowButton('decreaseABS', ui.Direction.Down, vec2(64,16)) then
-        if ABS > 0 then
-            ABS = ABS - 1
-            ac.setABS(ABS)
+    if car.absInAction then    
+        ui.image(Icons.ABS, vec2(64,64), rgbm(1,1,1,1), nil)
+    else
+        ui.image(Icons.ABS, vec2(64,64), rgbm(0,0,0,0.2), nil)
+        --ui.dummy(vec2(64,64))
+    end
+end
+
+local function changeTC()
+    local tcCurrent = car.tractionControlMode    
+    ac.debug('tcCurrent', tcCurrent)
+    ui.text('   TC')
+    if ui.arrowButton('increaseTC', ui.Direction.Up, vec2(64,16)) then
+        if tcCurrent < carModes.TC then
+            tcCurrent = tcCurrent + 1
+            ac.setTC(tcCurrent)
         end
+    end
+    ui.button(tcCurrent,vec2(64,24),ui.ButtonFlags.Disabled)
+    if ui.arrowButton('decreaseTC', ui.Direction.Down, vec2(64,16)) then
+        if tcCurrent > 0 then
+            tcCurrent = tcCurrent - 1
+            ac.setTC(tcCurrent)
+        end
+    end
+    ui.offsetCursor(vec2(75,-68))
+    if car.tractionControlInAction then
+        ui.image(Icons.ESC, vec2(64,64), rgbm(1,1,1,1), nil)
+    else
+        ui.image(Icons.ESC, vec2(64,64), rgbm(0,0,0,0.2), nil)
     end
 end
 
 local function rpmBar()
-
-end
-
---[[
-local function selectGear()
-    ui.text('   Gear')
-    if ui.arrowButton('increaseGear', ui.Direction.Up, vec2(64,16)) then
-        if Gear < 12 then
-            Gear = Gear + 1
-
-        end
-    end
-
-    ui.button(Gear,vec2(64,24))
-
-    if ui.arrowButton('decreaseGear', ui.Direction.Down, vec2(64,16)) then
-        if Gear > -1 then
-            Gear = Gear - 1
-        end
-    end
-end
-]]
-
-
-local squareOffset = 10
-local function drawWaterTemp()
-    local waterTemp = math.round(ac.getCar().waterTemperature/10, 0)
-    ac.debug('waterTemp', waterTemp)
-    local top_left, top_right, bottom_left, bottom_right = vec2(50,50)
-    for i = 1, waterTemp do
-      ui.drawQuadFilled(vec2(0+(i*50+squareOffset),0),vec2(50+(i*50),0),vec2(50+(i*50),100),vec2(0+(i*50+squareOffset),100), rgbm.colors.blue)
-    end
-end
-
-local barRotaion = 90
-function script.carStatus(dt)
-
-    local currentRPM = ac.getCar().rpm
+    local rpmCurrent = ac.getCar().rpm
     local maxRPM = ac.getCar().rpmLimiter
-    local rpmFraction = (currentRPM / maxRPM)
+    local rpmFraction = (rpmCurrent / maxRPM)
+    ac.debug('currentRPM', rpmCurrent)
 
     ui.progressBar(rpmFraction, vec2(ui.windowWidth(),20))
+end
 
-   --ui.childWindow('##waterTemp', vec2(100, 100), drawWaterTemp())
+local function selectGear()
+    gearCurrent = car.gear
+    ui.text('         Gear')
+    if ui.arrowButton('increaseGear', ui.Direction.Up, vec2(100,16)) then
+        if gearCurrent < carModes.gearCount then
+            gearCurrent = gearCurrent + 1
+
+        end
+    end
+    ui.pushFont((bit.bor(ui.Font.Huge,ui.Font.Monospace)))
+    ui.button(gearCurrent,vec2(100,70))
+    ui.popFont()
+
+    if ui.arrowButton('decreaseGear', ui.Direction.Down, vec2(100,16)) then
+        if gearCurrent > -1 then
+            gearCurrent = gearCurrent - 1
+        end
+    end
+end
+
+local function drawWaterTemp()
+    ac.debug('water temp',car.waterTemperature)
+
+    --Everything related to the initial dial state
+    local markColor = rgbm(1, 1, 1, 0.3)
+    local markColorGreen = rgbm(0.5, 1, 0.5, 0.7)
+    local markColorAmber = rgbm(1, 0.7, 0, 0.7)
+    local markColorRed = rgbm(1, 0, 0, 0.7)
+    local needleColor = rgbm(1, 1, 1, 1)
+    local dialStart, dialEnd = -0.5, 0.5
+
+    local lineCountLong = 6
+    local dialStartGreen = lineCountLong/3
+    local dialStartAmber = lineCountLong*2/3
+    local dialStartRed = lineCountLong/1
 
 
-   -- barRotaion = ui.slider('##barRotaion', barRotaion, -360,360,'%.f')
+    local dialRadius = 70
+    local offsetX = (table.sum(columnWidths) - columnWidths[5])+ dialRadius/2
+    local offsetY = dialRadius + 70
+    local dialCenter = vec2(offsetX+dialRadius,offsetY)
+
+    ui.drawCircleFilled(dialCenter, dialRadius, rgbm(0, 0, 0, 0.3), 50)
+
+        -- draws the larger indents on temp 
+
+    for i = 0, lineCountLong do 
+        local s = math.sin(math.lerp(dialStart, dialEnd, i / lineCountLong) * math.pi)
+        local c = -math.cos(math.lerp(dialStart, dialEnd, i / lineCountLong) * math.pi)
+        ui.drawLine(dialCenter + vec2(s, c) * (dialRadius-(dialRadius/3.5)), dialCenter + vec2(s, c) * (dialRadius), ((i >= dialStartGreen and i <= dialStartAmber) and markColorGreen) or ((i >= dialStartAmber and i < dialStartRed) and markColorAmber) or ((i >= dialStartRed) and markColorRed)or markColor, 1)
+    end
+
+    -- draws the small indents on temp 
+    local lineCountShort = lineCountLong*3
+    for i = 0, lineCountShort do 
+        if i % 3 ~= 0 then 
+            local s = math.sin(math.lerp(dialStart, dialEnd, i / lineCountShort) * math.pi)
+            local c = -math.cos(math.lerp(dialStart, dialEnd, i / lineCountShort) * math.pi)
+            ui.drawLine(dialCenter + vec2(s, c) * (dialRadius-(dialRadius/6)), dialCenter + vec2(s, c) * (dialRadius), ((i > dialStartGreen*3 and i < dialStartAmber*3) and markColorGreen) or ((i >= dialStartAmber*3 and i < dialStartRed*3-3) and markColorAmber) or ((i >= dialStartRed*3-3) and markColorRed)or markColor, 1)--((i > dialStartGreen*3 and i < 21) and markColorGreen) or ((i > 19 and i < 27) and markColorAmber) or ((i > 21) and markColorRed)or markColor, 1)
+        end
+    end
+
+    -- draws the needle
+    local angle = math.lerp(dialStart, dialEnd, math.lerpInvSat(car.waterTemperature, 60,90)) * math.pi
+    local s = math.sin(angle)
+    local c = -math.cos(angle)
+    ui.drawLine(dialCenter - vec2(s, c) * 0, dialCenter + vec2(s, c) * 70, needleColor, 1.5)
+
+    --now to draw the temp icons
+    ui.offsetCursor(vec2(dialRadius,dialRadius+25))
+    if car.waterTemperature < 65 then
+    ui.image(Icons.T_COLD, vec2(32,32), rgbm(1,1,1,1), nil)
+    elseif car.waterTemperature > 85 then
+    ui.image(Icons.T_HOT, vec2(32,32), rgbm(1,1,1,1), nil)
+    else
+    ui.image(Icons.T_COLD, vec2(32,32), rgbm(0,0,0,0.3), nil)
+    end
+
+end
+local timeVar = -1
+local counter = 0
+local spain = 0
+local function drawLightState()
+
+    --[[
+    if car.turningLeftLights then
+        if counter >= (timeVar + 1) then
+            if doClickOff then
+                ui.image(Icons.INDICATORS, vec2(64,64), rgbm(1,1,1,1), nil, vec2(0,0), vec2(0.5,1), true)
+                doClickOff = false
+            else
+                --ui.image(Icons.INDICATORS, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0,0), vec2(0.5,1), true)
+                doClickOff = true
+            end
+            timeVar = counter
+        else
+            ui.image(Icons.INDICATORS, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0,0), vec2(0.5,1), true)
+        end
+    else
+        ui.image(Icons.INDICATORS, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0,0), vec2(0.5,1), true)
+    end
+    ]]
+
+UIToggle = true 
+lastKeyState = false
+function script.drawUI()
+local keyState = ac.isKeyDown(ac.KeyIndex.H)
+    if keyState and lastKeyState ~= keyState then
+        UIToggle = not UIToggle
+        lastKeyState = keyState
+    elseif not keyState then
+        lastKeyState = false
+    end
+end
+
+
+
+
+    if car.turningLeftLights then
+        ui.image(Icons.INDICATORS, vec2(64,64), rgbm(1,1,1,1), nil, vec2(0,0), vec2(0.5,1), true)
+    else
+        ui.image(Icons.INDICATORS, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0,0), vec2(0.5,1), true)
+    end
+
+    ui.sameLine()
+
+    if car.turningRightLights then
+        ui.image(Icons.INDICATORS, vec2(64,64), rgbm(1,1,1,1), nil, vec2(0.5,0), vec2(1,1), true)
+    else
+        ui.image(Icons.INDICATORS, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0.5,0), vec2(1,1), true)
+    end
+
+
+
+    ui.image(Icons.LIGHT_PARKING, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0,0), vec2(1,1), true)
+    ui.sameLine()
+    --ui.image(Icons.LIGHT_PARKING, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0.5,0), vec2(1,1), true)
+    ui.image(Icons.LIGHT_HIGHBEAM, vec2(64,64), rgbm(0,0,0,0.3), nil, vec2(0,0), vec2(1,1), true)
+
+end
+
+
+function script.carStatus(dt)
+
+    rpmBar()
+
+    ui.separator()
+    -------------------------------------------------------
+    ui.columns(5,true, 'mainColumn')
+    ui.setColumnWidth(0, columnWidths[1])
+        changeABS()
+        changeTC()    
+    -------------------------------------------------------
+    ui.nextColumn()
+    ui.setColumnWidth(1, columnWidths[2])
+
+        drawLightState()
+    -------------------------------------------------------
+    ui.nextColumn()
+    ui.setColumnWidth(2, columnWidths[3])
+        selectGear()
+    -------------------------------------------------------
+    ui.nextColumn()
+    ui.setColumnWidth(3, columnWidths[4])
+        changeWiper()
+    -------------------------------------------------------
+    ui.nextColumn()
+    ui.setColumnWidth(4, columnWidths[5])
+        drawWaterTemp()
+    -------------------------------------------------------
+    ui.nextColumn()
+    ui.columns(1, false, 'none')
+    ui.separator()
+        susHeight = ui.slider('##susHeight', susHeight, 0,1,'%.2f')
+        ac.store('susHeight', susHeight)
+
+        counter = counter + dt
+end
+--    ui.colorPicker('##licenselight',plateColour)
+--    plateMult = ui.slider('##plateMult', plateMult, 0, 1000,'%.3f',1.5)
+--    valueSave = math.round(plateColour.r,2)..','..math.round(plateColour.g,2)..','..math.round(plateColour.b,2)..','..math.round(plateMult,2)
+--    ac.INIConfig.load('content/cars/acura_nsx_s1/extension/ext_config.ini'):setAndSave('LIGHT_LICENSEPLATE','COLOR',valueSave)
+
+    --susRear = ui.slider('##susRear', susRear, 0,1,'%.3f')
+    --ac.store('susRear', susRear)
+
+    --forceUp = ui.slider('##forceUp', forceUp, 0,20000,'%.3f')
+    --ac.store('forceUp', forceUp)
+
+    --ui.childWindow('##waterTemp', vec2(100, 100), drawWaterTemp())
+
+
+    -- barRotaion = ui.slider('##barRotaion', barRotaion, -360,360,'%.f')
 
     --[[for i = 1, rpmFraction*10 do
         ui.drawQuadFilled(vec2(0+(i*50+squareOffset),0),vec2(50+(i*50),0),vec2(50+(i*50),50),vec2(0+(i*50+squareOffset),50), rgbm.colors.red)
-    end]]
+    end
     --ui.beginRotation()
    -- drawWaterTemp()
-    --ui.endRotation(barRotaion)
---[[
-    ui.columns(5,true, 'mainColumn')
-    changeABS()
-    ui.nextColumn()
-    ui.nextColumn()
-    ui.nextColumn()
-    ui.nextColumn()
-        changeWiper()
+    --ui.endRotation(barRotaion)]]
 
 
-    ui.colorPicker('##licenselight',plateColour)
-    plateMult = ui.slider('##plateMult', plateMult, 0, 1000,'%.3f',1.5)
-    valueSave = math.round(plateColour.r,2)..','..math.round(plateColour.g,2)..','..math.round(plateColour.b,2)..','..math.round(plateMult,2)
-    ac.INIConfig.load('content/cars/acura_nsx_s1/extension/ext_config.ini'):setAndSave('LIGHT_LICENSEPLATE','COLOR',valueSave)
 
     --[[ac.debug('plateColour', plateColour)
     ac.debug('1r', math.round(plateColour.r,2))
     ac.debug('2g', math.round(plateColour.g,2))
     ac.debug('3b', math.round(plateColour.b,2))
     ac.debug('4mult', math.round(plateColour.mult,2))]]
-end
-
-
 
 --[[
 local plateColour = rgbm(10,10,10,10)
